925931e06457e5f89f10f193b0cfeaf30cafe280
Hosam Hasan <hosamkora@gmail.com>
2021-09-22 18:04:32 +0200

    make bootstrap command as normal as others

--- lib/commands/bootstrap.dart
+++ lib/commands/bootstrap.dart
@@ -1,25 +1,23 @@
-import 'package:riverpod/riverpod.dart';
-
 import '../configure_dependencies.dart';
 import '../services/logger.dart';
 import '../stores/font_icons_pacakges_store.dart';
 import 'get_font_icons_packages.dart';
 import 'icon_commnds/load_fonts.dart';
 
-final bootstrabProvider = StreamProvider((ref) async* {
-  final logger = getIt<Logger>();
+class Bootstrap {
+  Stream<String> call() async* {
+    final logger = getIt<Logger>();
 
-  logger.i('bootstraping....');
+    logger.i('bootstraping....');
 
-  yield 'Loading All Icons....';
-  final packages = await GetFontIconsPacakges().call();
-  ref.read(fontIconsPackagesStoreProvider).init(packages);
+    yield 'Loading All Icons....';
+    final packages = await GetFontIconsPacakges().call();
+    getIt<FontIconsPackagesStore>().init(packages);
 
-  yield 'Loading All Fonts...';
-  await LoadFonts().call(packages);
+    yield 'Loading All Fonts...';
+    await LoadFonts().call(packages);
 
-  yield 'Done';
-  logger.i('bootstraping ✅');
-
-  await Future<void>.delayed(const Duration(seconds: 1));
-});
+    yield 'Done';
+    logger.i('bootstraping ✅');
+  }
+}

77a23ea3e164b909123aa7325293cbbb4940a89c
Hosam Hasan <hosamkora@gmail.com>
2021-09-27 18:18:20 +0200

    inject global state with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 import 'configure_dependencies.dart';
+import 'stores/font_icons_pacakges_store.dart';
 import 'views/pages/intro_page.dart';
 
 void main() async {
   await configureDependencies();
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => getIt<FontIconsPackagesStore>())
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
